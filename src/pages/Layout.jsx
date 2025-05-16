098affaa2498dca05358cac99f9a65b3cfeaed33
kamilavieira <kamilinhas.vieira@gmail.com>
2025-05-15 21:34:09 -0300

    Foi feita a estrutura do 'ProductCard'para ter uma versão salva. Será alterado posteriormente com ajustes de acordo com o dev figma.

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Section from "../components/Section";
+import ProductCard from "../components/ProductCard";
 
 const Layout = ({children}) => {
     return ( 
@@ -9,6 +10,7 @@ const Layout = ({children}) => {
             <Header />
             <main>
                 <Section />
+                <ProductCard />
                 {children}
             </main>
             <Footer />
