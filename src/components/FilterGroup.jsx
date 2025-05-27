5d6de23e4e8e1a9efa13e0d0d460e2f368b88196
kamilavieira <kamilinhas.vieira@gmail.com>
2025-05-27 19:37:57 -0300

    ajustei o margin do filtro

--- src/components/FilterGroup.jsx
+++ src/components/FilterGroup.jsx
@@ -2,7 +2,7 @@ import React, { Children } from "react";
 
 const FilterGroup = ({ title, inputType = "checkbox", options = [] }) => {
   return (
-    <div className="h-60 w-308 p-15 flex flex-col bg-white rounded justify-start items-start">
+    <div className="m-7 h-308 w-60 p-15 flex flex-col bg-white rounded justify-start items-start">
       <h3 className="filter-title">{title}</h3>
       {options.map((opt, index) => (
         <label key={index} className="filter-option">
